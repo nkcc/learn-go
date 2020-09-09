801f8bf91404c7aeca0529bc072b16a8e14ed64c
alex chen <alexchen@Alex-chen.local>
2020-09-09 17:00:33 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	_ "./hello"
+)
+
+func main() {
+	buf := make([]byte, 1024)
+	f, _ := os.Open("/Users/alexchen/Code/laradock/README1.md")
+	defer f.Close()
+
+	for {
+		n, _ := f.Read(buf)
+		if n == 0 {
+			break
+		}
+
+		os.Stdout.Write(buf[:n])
+	}
+}
